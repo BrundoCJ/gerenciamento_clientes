8bb8d87913341864c7572556f7c193a705a80d5b
BrundoCJ <bconrado04@gmail.com>
2026-03-24 16:52:50 -0300

    adicionando repository

--- demo/src/main/java/com/gerenciamento_de_clientes/demo/repository/ClienteRepository.java
+++ demo/src/main/java/com/gerenciamento_de_clientes/demo/repository/ClienteRepository.java
@@ -1,4 +1,9 @@
 package com.gerenciamento_de_clientes.demo.repository;
 
-public class ClienteRepository {
+import com.gerenciamento_de_clientes.demo.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
