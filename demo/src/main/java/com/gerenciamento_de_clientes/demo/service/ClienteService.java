bc128d7d96fd27cd98e670234f4a3f5501de3c5b
BrundoCJ <bconrado04@gmail.com>
2026-03-24 16:53:16 -0300

    adicionando service

--- demo/src/main/java/com/gerenciamento_de_clientes/demo/service/ClienteService.java
+++ demo/src/main/java/com/gerenciamento_de_clientes/demo/service/ClienteService.java
@@ -1,4 +1,38 @@
 package com.gerenciamento_de_clientes.demo.service;
 
+import com.gerenciamento_de_clientes.demo.entity.Cliente;
+import com.gerenciamento_de_clientes.demo.repository.ClienteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public Cliente salvar(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> listarTodos() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> buscarPorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public boolean deletar(Long id) {
+        if (clienteRepository.existsById(id)) {
+            clienteRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
